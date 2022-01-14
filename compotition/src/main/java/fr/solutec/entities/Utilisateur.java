a5f93e2e87973255ae2e6d9fb657eb3073e0e382
Spongebobs3000 <you@example.com>
2022-01-14 12:01:02 +0100

    getInformationsUtilisateurById setInformationsUtilisateur

--- compotition/src/main/java/fr/solutec/entities/Utilisateur.java
+++ compotition/src/main/java/fr/solutec/entities/Utilisateur.java
@@ -2,6 +2,7 @@ package fr.solutec.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -18,10 +19,12 @@ public class Utilisateur {
 	@Id
 	@GeneratedValue
 	private Long id;
+	@Column(nullable = false, unique = true) // contrainte d'unicité
 	private String login;
 	private String mdp;
 	private String nom;
 	private String prenom;
+	@Column(unique = true)
 	private String email;
 	private Date dateNaissance;
 	private boolean admin;
--- compotition/src/main/java/fr/solutec/rest/UtilisateurRest.java
+++ compotition/src/main/java/fr/solutec/rest/UtilisateurRest.java
@@ -4,7 +4,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +27,16 @@ public class UtilisateurRest {
 
 	@PostMapping("inscription")
 	public Utilisateur inscription(@RequestBody Utilisateur u) throws Exception {
+		return utilisateurRepos.save(u);
+	}
+
+	@GetMapping("informations-utilisateur/{id}")
+	public Optional<Utilisateur> getInformationsUtilisateurById(@PathVariable Long id) {
+		return utilisateurRepos.findById(id);
+	}
+
+	@PutMapping("modifier-informations-utilisateur")
+	public Utilisateur setInformationsUtilisateur(@RequestBody Utilisateur u) {
 		return utilisateurRepos.save(u);
 	}
 
