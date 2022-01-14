e9fde45989ef620dbcf5dfb6f03a8bf679572e24
Spongebobs3000 <you@example.com>
2022-01-14 10:05:06 +0100

    connexion utilisateur

--- compotition/src/main/java/fr/solutec/rest/UtilisateurRest.java
+++ compotition/src/main/java/fr/solutec/rest/UtilisateurRest.java
@@ -1,15 +1,25 @@
 package fr.solutec.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.Utilisateur;
 import fr.solutec.repository.UtilisateurRepository;
 
 @RestController
 @CrossOrigin("*")
 public class UtilisateurRest {
 	@Autowired
-	private UtilisateurRepository utilisateurRepo;
+	private UtilisateurRepository utilisateurRepos;
+
+	@PostMapping("connexion")
+	public Optional<Utilisateur> connexion(@RequestBody Utilisateur u) {
+		return utilisateurRepos.findByLoginAndMdp(u.getLogin(), u.getMdp());
+	}
 
 }
