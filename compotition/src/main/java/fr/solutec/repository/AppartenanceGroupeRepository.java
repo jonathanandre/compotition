2522b468b085497a87571d6e7e04416fb065c818
Spongebobs3000 <you@example.com>
2022-01-14 15:52:38 +0100

    Appartenance groupe

--- compotition/src/main/java/fr/solutec/repository/AppartenanceGroupeRepository.java
+++ compotition/src/main/java/fr/solutec/repository/AppartenanceGroupeRepository.java
@@ -1,9 +1,14 @@
 package fr.solutec.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.AppartenanceGroupe;
+import fr.solutec.entities.Groupe;
 
 public interface AppartenanceGroupeRepository extends CrudRepository<AppartenanceGroupe, Long> {
+
+	public List<Groupe> findByUtilisateurId(Long id);
 
 }
--- compotition/src/main/java/fr/solutec/rest/GroupeRest.java
+++ compotition/src/main/java/fr/solutec/rest/GroupeRest.java
@@ -1,10 +1,23 @@
 package fr.solutec.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.entities.Groupe;
+import fr.solutec.repository.GroupeRepository;
 
 @RestController
 @CrossOrigin("*")
 public class GroupeRest {
+	@Autowired
+	private GroupeRepository groupeRepos;
+
+	@PostMapping("groupe/creer")
+	public Groupe creerGroupe(@RequestBody Groupe g) {
+		return groupeRepos.save(g);
+	}
 
 }
