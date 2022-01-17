d534a27465fa42073bccbe930f0958d91edc66de
Spongebobs3000 <you@example.com>
2022-01-17 16:26:39 +0100

    duel

--- compotition/src/main/java/fr/solutec/repository/DuelRepository.java
+++ compotition/src/main/java/fr/solutec/repository/DuelRepository.java
@@ -1,9 +1,18 @@
 package fr.solutec.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Duel;
 
 public interface DuelRepository extends CrudRepository<Duel, Long> {
 
+	public List<Duel> findByAdversaire1LoginOrAdversaire2Login(String login, String login2);
+
+	public List<Duel> findByCompetitionId(Long id);
+
+	public Duel findByCompetitionIdAndAdversaire1LoginAndAdversaire2Login(Long id, String login, String login2);
+
+	public List<Duel> findByCompetitionIdAndAdversaire1LoginOrAdversaire2Login(Long id, String login, String login2);
 }
