e0dd7ae6ccc68b8b1f0057cf7bef720aace77bfb
Spongebobs3000 <you@example.com>
2022-01-19 11:47:31 +0100

    commentaire

--- compotition/src/main/java/fr/solutec/rest/ParticipeRest.java
+++ compotition/src/main/java/fr/solutec/rest/ParticipeRest.java
@@ -36,13 +36,13 @@ public class ParticipeRest {
 		return participeRepos.findByUtilisateurLogin(login);
 	}
 
-	// FONCTIONNE PAS
+	// Avec id compétition et login utilisateur
 	@GetMapping("competition/{id}/participation/utilisateur/{login}")
 	public Optional<Participe> getUniqueParticiaption(@PathVariable Long id, @PathVariable String login) {
 		return participeRepos.findByCompetitionIdAndUtilisateurLogin(id, login);
 	}
 
-	// FONCTIONNE PAS Avec id compétition et login utilisateur
+	// Avec id compétition et login utilisateur
 	@DeleteMapping("competition/{id}/participation/supprimer/{login}")
 	public boolean deleteUtilisateur(@PathVariable Long id, @PathVariable String login) {
 		Optional<Participe> p = participeRepos.findByCompetitionIdAndUtilisateurLogin(id, login);
